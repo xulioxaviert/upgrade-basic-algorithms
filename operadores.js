4cc13664a9a5859866b1160262d542638d6209ed
Xulio Xaviert Rojas Teixeira <xulioxaviert@gmail.com>
2022-02-28 16:13:13 +0100

    Iteración #3: Operadores

---
+++ operadores.js
@@ -0,0 +1,55 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+let numA = 10;
+let numB = 5;
+
+let resulMulti = numA * numB;
+
+alert('El resultado de multiplicar ' + numA + ' * ' + numB + ' es ' + resulMulti);
+
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+let numC = 10;
+let numD = 2;
+
+let resulDiv = numC / numD;
+
+alert('El resultado de dividir ' + numC + ' / ' + numD + ' es ' + resulDiv);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+let numE = 15;
+let numF = 9;
+
+let resulResto = numE % numF;
+
+alert('El resto de la división ' + numE + ' % ' + numF + ' es ' + resulResto);
+
+
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15,
+// teniendo dos variables y = 10 y z = 5.
+
+let x;
+
+let y = 10;
+let z = 5;
+
+x = y + z;
+
+console.log('El operador correcto es ' + '+');
+
+
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+let x;
+
+let y = 10;
+let z = 5;
+
+x = y * z;
+
+console.log('El operador correcto es ' + '*');
